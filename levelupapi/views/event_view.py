d20b9254be268392b42637a76c75eb5161314c39
Ryan Phillips <ryanmphill@gmail.com>
2023-08-08 14:32:47 -0500

    Retrieved events from db with expanded organizer and game

---
+++ levelupapi/views/event_view.py
@@ -0,0 +1,57 @@
+"""View module for handling requests about games"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers, status
+from levelupapi.models import Game, Gamer, Event
+
+
+class EventView(ViewSet):
+    """Level up events view"""
+
+    def retrieve(self, request, pk):
+        """Handle GET requests for single event
+
+        Returns:
+            Response -- JSON serialized event
+        """
+        event = Event.objects.get(pk=pk)
+        serializer = EventSerializer(event)
+        return Response(serializer.data)
+
+
+    def list(self, request):
+        """Handle GET requests to get all events
+
+        Returns:
+            Response -- JSON serialized list of events
+        """
+        events = Event.objects.all()
+        serializer = EventSerializer(events, many=True)
+        return Response(serializer.data)
+
+class GameOfEventSerializer(serializers.ModelSerializer):
+    """JSON serializer for gamer
+    """
+    class Meta:
+        model = Game
+        fields = ('id', 'name', 'game_type', 'creator')
+
+class EventOrganizerSerializer(serializers.ModelSerializer):
+    """JSON serializer for gamer
+    """
+    class Meta:
+        model = Gamer
+        fields = ('id', 'user', 'bio')
+
+class EventSerializer(serializers.ModelSerializer):
+    """JSON serializer for games
+    """
+
+    game = GameOfEventSerializer(many=False)
+    organizer = EventOrganizerSerializer(many=False)
+
+    class Meta:
+        model = Event
+        fields = ('id', 'title', 'organizer', 'game', 'date', 'attendees')
+        depth = 1

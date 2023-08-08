e7c2f4b7f5b03cf75c96f18f81606ca584027065
Ryan Phillips <ryanmphill@gmail.com>
2023-08-08 13:45:42 -0500

    Retrieved gametypes from database

--- levelup/urls.py
+++ levelup/urls.py
@@ -17,10 +17,16 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
-from levelupapi.views import register_user, login_user
+from levelupapi.views import register_user, login_user, GameTypeView
+from rest_framework import routers
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'gametypes', GameTypeView, 'gametype')
+
 
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]

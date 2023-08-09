e1ec86f7c4dbfd00b808ebf7e4c999f65382ea58
Ryan Phillips <ryanmphill@gmail.com>
2023-08-09 14:48:52 -0500

    Added update method to event view

--- levelupapi/views/event_view.py
+++ levelupapi/views/event_view.py
@@ -53,6 +53,28 @@ class EventView(ViewSet):
         )
         serializer = EventSerializer(event)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+    
+    def update(self, request, pk):
+        """Handle PUT requests for a event
+
+        Returns:
+            Response -- Empty body with 204 status code
+        """
+
+        event = Event.objects.get(pk=pk)
+        event.title = request.data["title"]
+
+        organizer = Gamer.objects.get(pk=request.data["organizer"])
+        event.organizer = organizer
+
+        game = Game.objects.get(pk=request.data["game"])
+        event.game = game
+
+        event.date = request.data["date"]
+
+        event.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 class GameOfEventSerializer(serializers.ModelSerializer):
     """JSON serializer for gamer
